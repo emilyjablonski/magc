3cfd40e3b9bc5a10925088e82bbe537f3b30b537
Emily Jablonski <emily.jablonski@exygy.com>
2023-04-01 14:39:23 -0600

    dropdown menus

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,35 +1,98 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./NavigationBar.module.scss"
 import { GiHamburgerMenu } from "react-icons/gi"
 
 type NavigationBarProps = {}
 
-const NavItem = (props: { item: string }) => {
-  return <span className={styles["navigation-bar-item"]}>{props.item}</span>
-}
+const navigationItems: Array<{ [key: string]: string[] }> = [
+  { MAGC: [] },
+  {
+    About: [
+      "History of MAGC",
+      "Vision & Mission",
+      "Board of Directors",
+      "Strategic Plan and Funding Efforts",
+      "Committees",
+    ],
+  },
+  { "Annual Conference": ["Attend Conference", "Sponsorship Information"] },
+  { Membership: ["Membership Types & Benefits", "Become a Member", "Member Login"] },
+  {
+    "About GCs": [
+      "What is a Genetic Counselor?",
+      "Prospective Students",
+      "Genetic Services in Michigan",
+    ],
+  },
+  { "News & Events": [] },
+  { Jobs: [] },
+  { "Contact Us": [] },
+]
 
 const NavigationBar = (props: NavigationBarProps) => {
-  const navigationItems = [
-    "MAGC",
-    "About",
-    "Annual Conference",
-    "Membership",
-    "About GCs",
-    "News & Events",
-    "Jobs",
-    "Contact Us",
-  ]
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null)
+
+  const onNavItemMouseEnter = (title: string) => {
+    setOpenDropdown(title)
+  }
+
+  const onNavItemMouseLeave = (title: string) => {
+    setOpenDropdown(null)
+  }
+
+  const NavItem = (props: { title: string }) => {
+    return (
+      <span
+        className={styles["navigation-bar-item"]}
+        onMouseEnter={() => onNavItemMouseEnter(props.title)}
+        onMouseLeave={() => onNavItemMouseLeave(props.title)}
+      >
+        {props.title}
+      </span>
+    )
+  }
+
+  const DropdownItem = (props: { title: string }) => {
+    return <span className={styles["navigation-bar-dropdown-item"]}>{props.title}</span>
+  }
+
+  const DropdownMenu = (props: { parentTitle: string; children: React.ReactNode }) => {
+    if (props.parentTitle !== openDropdown) return null
+    return (
+      <div
+        className={styles["navigation-bar-dropdown"]}
+        onMouseEnter={() => onNavItemMouseEnter(props.parentTitle)}
+        onMouseLeave={() => onNavItemMouseLeave(props.parentTitle)}
+      >
+        {props.children}
+      </div>
+    )
+  }
+
   return (
     <nav className={styles["navigation-bar"]}>
       <div className={styles["navigation-bar-content"]}>
         <div className={styles["navigation-bar-left"]}>
-          {navigationItems.map((text) => {
-            return <NavItem item={text} />
+          {navigationItems.map((navItem) => {
+            const title = Object.keys(navItem)[0]
+
+            return (
+              <div className={styles["navigation-item-container"]}>
+                <NavItem title={title} />
+                {!!navItem[title].length && (
+                  <DropdownMenu parentTitle={title}>
+                    {navItem[title].map((dropdownItem) => {
+                      return <DropdownItem title={dropdownItem} />
+                    })}
+                  </DropdownMenu>
+                )}
+              </div>
+            )
           })}
         </div>
         <div className={styles["navigation-bar-right"]}>
           <div className={styles["navigation-bar-desktop"]}>
-            <NavItem item={"Login"}></NavItem>
+            <NavItem title={"Login"}></NavItem>
           </div>
           <div className={styles["navigation-bar-mobile"]}>
             <GiHamburgerMenu />
