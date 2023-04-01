761e8c7da0f6934c674a3ea595e5f4c6d9aa183a
Emily Jablonski <emily.jablonski@exygy.com>
2023-04-01 12:19:01 -0600

    updating styling

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -10,13 +10,20 @@ const NavItem = (props: { item: string }) => {
 const NavigationBar = (props: NavigationBarProps) => {
   return (
     <nav className={styles["navigation-bar"]}>
-      <NavItem item={"MAGC"}></NavItem>|<NavItem item={"About Us"}></NavItem>
-      <NavItem item={"Annual Conference"}></NavItem>
-      <NavItem item={"Membership"}></NavItem>
-      <NavItem item={"About Genetic Counselors"}></NavItem>
-      <NavItem item={"News & Events"}></NavItem>
-      <NavItem item={"Job Board"}></NavItem>
-      <NavItem item={"Contact Us"}></NavItem>
+      <div className={styles["navigation-bar-content"]}>
+        <div className={styles["navigation-bar-items"]}>
+          <NavItem item={"MAGC"}></NavItem>|<NavItem item={"About Us"}></NavItem>
+          <NavItem item={"Annual Conference"}></NavItem>
+          <NavItem item={"Membership"}></NavItem>
+          <NavItem item={"About Genetic Counselors"}></NavItem>
+          {/* <NavItem item={"News & Events"}></NavItem>
+        <NavItem item={"Job Board"}></NavItem>
+        <NavItem item={"Contact Us"}></NavItem> */}
+        </div>
+        <div className={styles["navigation-bar-login"]}>
+          <NavItem item={"Login"}></NavItem>
+        </div>
+      </div>
     </nav>
   )
 }
